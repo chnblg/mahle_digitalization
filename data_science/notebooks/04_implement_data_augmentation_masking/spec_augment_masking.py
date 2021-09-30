b850f7ccd7fb0763c680acad99daf95c89dac8d7
chnblg <82374525+chnblg@users.noreply.github.com>
2021-09-30 13:18:08 +0200

    Add files via upload

---
+++ data_science/notebooks/04_implement_data_augmentation_masking/spec_augment_masking.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pylab as plt 
+import warnings
+import pandas as pd
+import os
+
+from librosa import display
+from processing import preprocessing, data_augmentation
+
+
+# In[2]:
+
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+warnings.filterwarnings("ignore")
+
+
+# In[3]:
+
+
+data_df = pd.read_json('/datasets/UrbanSound8K/processed/mean_mfcc_data.json')
+data_df = preprocessing.filter_mfccs(data_df)
+
+
+# In[4]:
+
+
+i = 1
+print(data_df.label.iloc[i])
+display.specshow(data_df.feature.iloc[i], x_axis='time', y_axis='mel')
+plt.colorbar(format='%+2.0f dB')
+plt.show()
+
+
+# In[5]:
+
+
+for i in range(50):
+    print(data_df.label.iloc[i])
+    display.specshow(data_augmentation.apply_time_and_frequency_masking_to_spectrum(data_df.feature.iloc[i]),
+                     x_axis='time', y_axis='mel')
+    plt.colorbar(format='%+2.0f dB')
+    plt.show()
+
+
+# In[ ]:
+
+
+
+
